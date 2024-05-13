6ee677e43a0f5b7bf32af4cd8df4233028724aee
JacopoLombardi <lombajacopo@gmail.com>
2024-05-13 15:34:55 +0200

    [add] Migration of table completed

---
+++ database/migrations/2024_05_13_131214_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 30);
+            $table->string('departure_station', 40);
+            $table->string('arrival_station', 40);
+            $table->time('departure_time');
+            $table->time('arrival time');
+            $table->char('train_number', 10)->unique();
+            $table->tinyInteger('carriage_number');
+            $table->boolean('is_on_time')->default(true);
+            $table->boolean('canceled')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
